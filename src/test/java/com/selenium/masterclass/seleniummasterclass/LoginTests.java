72b75c12d56c570a9503575bb35b314581d84902
Gaurav G Raj <gauravgraj91@gmail.com>
2018-12-17 21:23:12 +0530

    Adding capabilities to read and write data from excel sheet and modified a few tests.

--- src/test/java/com/selenium/masterclass/seleniummasterclass/LoginTests.java
+++ src/test/java/com/selenium/masterclass/seleniummasterclass/LoginTests.java
@@ -11,16 +11,21 @@ public class LoginTests extends TestBase{
     public void launchTestOne() throws Exception{
         driver.get("http://www.amazon.in");
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.get("https://www.amazon.in/amazonprime?_encoding=UTF8&ref=nav_logo_prime_join");
-        /*WebElement goToTryPrime = driver.findElement(By.name("Try Prime"));
-        goToTryPrime.click();*/
         Thread.sleep(4000);
+        WebElement explorePrimeBtn = driver.findElement(By.xpath("//a[@class='nav-sprite nav-logo-tagline nav-prime-try']"));
+        explorePrimeBtn.click();
+        WebElement homeBtn = driver.findElement(By.xpath("//span[@class='nav-sprite nav-logo-base']"));
+        homeBtn.click();
+        driver.navigate().back();
     }
 
     @Test(description = "Selecting an option from dropdown", dependsOnMethods = "launchTestOne")
     public void dropDownElementMore() throws Exception{
-        WebElement dropdownMoreBtn = driver.findElement(By.className("nav-logo-link"));
-        dropdownMoreBtn.click();
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox'] "));
+        searchBox.click();
+        searchBox.sendKeys("Portronics");
+        WebElement searchBtn = driver.findElement(By.xpath("//input[@value='Go']"));
+        searchBtn.click();
         log.debug("The first test case passed successfully.");
         Thread.sleep(3000);
     }
