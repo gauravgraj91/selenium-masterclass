6ecd871af98387893857bcdcf70d5b97f35cee73
Gaurav G Raj <gauravgraj91@gmail.com>
2019-01-03 23:40:13 +0530

    Making modifications to tests and practicing the usage of Select statement.

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # Selenium-Masterclass
-Practicing selenium framework modules.
+
+Practicing selenium framework modules and also scenarios that we come across while handling different elements on the Web.
 
 # Modules Implemented
 
-* Log4j integration
-* Extent Reports
-* Custom Listeners for Test cases logs.
+* Log4j integrated
+* Extent Reports implemented
+* Created Base class with all necessary abstract methods.
+* Custom Listeners for Test cases with logs.
 * Excel Integration with Read and Write capabilities.
 
 # Local Repo
-cd ../../..
-cd C:\Users\Gaurav\Desktop\Projects\selenium-masterclass
+* cd ../../..
+* cd C:\Users\Gaurav\Desktop\Projects\selenium-masterclass
 
 # Ref
-git add .
-git commit -m "message"
-git push origin master
+* git add .
+* git commit -m "message"
+* git push origin master
--- src/test/java/com/selenium/masterclass/seleniummasterclass/LoginTests.java
+++ src/test/java/com/selenium/masterclass/seleniummasterclass/LoginTests.java
@@ -1,20 +1,21 @@
 package com.selenium.masterclass.seleniummasterclass;
 
 import com.selenium.masterclass.seleniummasterclass.Base.TestBase;
-import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class LoginTests extends TestBase {
 
-    @Test(description = "This is for Testing")
+    @Test(description = "Launching Website and testing a few elements on the page.")
     public void websiteLaunchTest() throws Exception {
         driver.get("http://www.amazon.in");
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         Thread.sleep(4000);
+        Assert.assertEquals(driver.getTitle(),"Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
         WebElement explorePrimeBtn = driver.findElement(By.xpath("//a[@class='nav-sprite nav-logo-tagline nav-prime-try']"));
         Assert.assertTrue(isElementPresent(By.xpath("//a[@class='nav-sprite nav-logo-tagline nav-prime-try']")));
         explorePrimeBtn.click();
@@ -24,8 +25,8 @@ public class LoginTests extends TestBase {
         log.debug("Launching browser and routing to Website test passed");
     }
 
-    @Test(description = "Selecting an option from dropdown", dependsOnMethods = "websiteLaunchTest")
-    public void searchElement() throws Exception {
+    @Test(description = "Searching an product in the search box.", dependsOnMethods = "websiteLaunchTest")
+    public void searchProduct() throws Exception {
         WebElement searchBox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox'] "));
         searchBox.click();
         searchBox.sendKeys("Portronics");
@@ -33,5 +34,17 @@ public class LoginTests extends TestBase {
         searchBtn.click();
         log.debug("Search element testcase passed successfully.");
         Thread.sleep(3000);
+    }
+
+    @Test(description = "Selecting an element from dropdown", dependsOnMethods = "searchProduct")
+    public void selectElementFromDropDown() throws Exception{
+        Select sortProducts = new Select(driver.findElement(By.id("sort")));
+        sortProducts.selectByVisibleText("Price: Low to High");
+        driver.navigate().back();
+        Thread.sleep(4000);
+        /*sortProducts.selectByIndex(2);
+        Thread.sleep(4000);
+        sortProducts.selectByVisibleText("Avg. Customer Review");
+        sortProducts.selectByValue("date-desc-rank");*/
     }
 }
